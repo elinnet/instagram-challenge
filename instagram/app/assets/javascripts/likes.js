be376c75092b87e1037569100efae5b7632bc0d8
Elin <elinnet@gmail.com>
2015-07-28 01:00:09 +0100

    Lasted changes to have page update with count of likes - test still fails

--- instagram/app/assets/javascripts/likes.js
+++ instagram/app/assets/javascripts/likes.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-  $('.likes-link').on('click', function(event){
+  $('.likes-link').on('click', function(event){ //when user clikcs 'likes' link
       event.preventDefault();
 
-      var likeCount = $(this).siblings('.likes_count');
+      var likeCount = $(this).siblings('.likes_count'); //this is the likes counter
 
-      $.post(this.href, function(response){
+      $.post(this.href, function(response){ //this.href is URL request is being sent to - what link is this?
         likeCount.text(response.new_like_count); //not sure what this last bit means
+
     });
   });
 });
--- instagram/app/controllers/likes_controller.rb
+++ instagram/app/controllers/likes_controller.rb
@@ -1,9 +1,12 @@
 class LikesController < ApplicationController
 
   def create
-    @picture = Picture.find(params[:picture_id])
-    @picture.likes.create
+    @picture = Picture.find(params[:picture_id]) #find the id of the picture
+    @picture.likes.create #create a like against the picture
+
     render json: {new_like_count: @picture.likes.count}
+    #count the likes on the picture, make this the value against the key of 'new_like_count', render this in json?
+
   end
 
 end
